a867b0a049d9acc4dd3ba7c687ac1c56642f54fc
jrmarzu <44399371+jrmarzu@users.noreply.github.com>
2019-04-24 12:23:52 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friendMatch = [
+    {
+      name: "Squidward",
+      photo: "https://playplex.mtvnimages.com/uri/mgid:arc:content:nick.intl:78fbb26a-6ced-4c10-9d85-876d514def90?stage=live&ep=nick.tv",
+      scores: ["2", "2", "2", "2", "2"]
+    }, {
+      name: "Gary The Snail",
+      photo: "http://en.spongepedia.org/images/thumb/1/19/SpongeBob_SquarePants_Gary.jpg/250px-SpongeBob_SquarePants_Gary.jpg",
+      scores: ["3", "3", "3", "3", "3"]
+    }, {
+      name: "SpongeBob Square Pants",
+      photo: "https://blog.lboro.ac.uk/freshers/wp-content/uploads/sites/25/2017/08/spongebob-squarepants-8855-1920x1200.jpg",
+      scores: ["1", "1", "1", "1", "1"]
+    }, {
+      name: "Patrick Star",
+      photo: "https://quotecatalog.imgix.net/assets/asset-99p09AugbOHnUoX609cRmSZ3/original.jpg?fit=facearea&w=680&h=415&facepad=4.5",
+      scores: ["3", "3", "3", "3", "1"]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
+  
